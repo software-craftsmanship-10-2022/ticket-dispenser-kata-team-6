57f61b0472c5c93ff13f7b6add0ef3a95823dcdf
Alex Park <a.park@tipsterchat.com>
2022-11-29 15:11:20 +0100

    add first approach to tests

--- src/ticket-dispenser.test.ts
+++ src/ticket-dispenser.test.ts
@@ -1,9 +1,25 @@
 import TicketDispenser from "./ticket-dispenser";
 
+/**
+ * THINGS: turn number sequence does not reset after each test.
+ * We can do an afterAll, but that involves mocking the class as well.
+ */
+
 describe("[TicketDispenser]", () => {
   it("should return a new ticket", () => {
     const dispenser = new TicketDispenser();
     const ticket = dispenser.getTurnTicket();
-    expect(ticket.getTurnNumber()).toBe(-1);
+    expect(ticket.getTurnNumber()).toBe(0);
+  });
+
+  it("should be able to handle multiple ticket dispensers", () => {
+    const firstDispenser = new TicketDispenser();
+    const secondDespenser = new TicketDispenser();
+
+    const firstTicket = firstDispenser.getTurnTicket();
+    const secondTicket = secondDespenser.getTurnTicket();
+
+    expect(firstTicket.getTurnNumber()).toBe(0);
+    expect(secondTicket.getTurnNumber()).toBe(1);
   });
 });
